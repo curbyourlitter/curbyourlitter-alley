13f5e53e6a86837e697a4ab2a6beec2f4298a5bf
Eric Brelsford <ebrelsford@gmail.com>
2015-08-28 12:42:48 -0400

    Working version of cartodbsync

--- scripts/cartodbsync.sh
+++ scripts/cartodbsync.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "Starting cartodbsync"
 source $HOME/.bash_profile
-source `which virtualenvwrapper.sh`
-workon $ALLEY_VIRTUAL_ENV
-# TODO ARGHGEGEGEGEJJKSLDFJKL
+workon alley
 django-admin cartodbsync
 echo "Finishing cartodbsync"
