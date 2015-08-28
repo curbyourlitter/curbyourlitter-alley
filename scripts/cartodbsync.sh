16c7b6351fd27276980e4c6c6f38271a556bcd9c
Eric Brelsford <ebrelsford@gmail.com>
2015-08-28 00:25:11 -0400

    Echo

--- scripts/cartodbsync.sh
+++ scripts/cartodbsync.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "Starting cartodbsync"
 source $HOME/.bash_profile
 source `which virtualenvwrapper.sh`
 workon $ALLEY_VIRTUAL_ENV
 django-admin cartodbsync
+echo "Finishing cartodbsync"
