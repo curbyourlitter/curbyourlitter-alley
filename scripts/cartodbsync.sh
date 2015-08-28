973c15e409cfa9c55adb1f929a5fa592ccacdffd
Eric Brelsford <ebrelsford@gmail.com>
2015-08-28 00:23:06 -0400

    Fix cartodbsync.sh

--- scripts/cartodbsync.sh
+++ scripts/cartodbsync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source $HOME/.bash_profile
-workon $ALLEY_VIRTUAL_ENV && django-admin cartodbsync
+source `which virtualenvwrapper.sh`
+workon $ALLEY_VIRTUAL_ENV
+django-admin cartodbsync
