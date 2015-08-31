2ef37f278f8b69ab87e5a2d6059f6dac66d2d654
Eric Brelsford <ebrelsford@gmail.com>
2015-08-31 17:07:53 -0400

    Better admin views for socratasync

--- curbyourlitter_alley/socratasync/admin.py
+++ curbyourlitter_alley/socratasync/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from .models import CartodbTable, Connection, SocrataResource
 
 
-admin.site.register(CartodbTable)
+class CartodbTableAdmin(admin.ModelAdmin):
+    list_display = ('table', 'domain',)
+
+
+class SocrataResourceAdmin(admin.ModelAdmin):
+    list_display = ('endpoint', 'domain',)
+
+
+admin.site.register(CartodbTable, CartodbTableAdmin)
 admin.site.register(Connection)
-admin.site.register(SocrataResource)
+admin.site.register(SocrataResource, SocrataResourceAdmin)
--- curbyourlitter_alley/socratasync/models.py
+++ curbyourlitter_alley/socratasync/models.py
@@ -8,11 +8,17 @@ class SocrataResource(models.Model):
     token = models.CharField(max_length=50)
     unique_key = models.CharField(max_length=50)
 
+    def __str__(self):
+        return '{}{}'.format(self.domain, self.endpoint)
+
 
 class CartodbTable(models.Model):
     apiKey = models.CharField(max_length=100)
     domain = models.CharField(max_length=100)
     table = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.table)
 
 
 class Connection(models.Model):
@@ -23,3 +29,6 @@ class Connection(models.Model):
     def get_adapter(self):
         from . import adapters
         return getattr(adapters, self.adapter)
+
+    def __str__(self):
+        return '{} -> {}'.format(self.socrata_resource, self.cartodb_table)
