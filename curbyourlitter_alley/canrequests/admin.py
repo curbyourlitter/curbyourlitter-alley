e22ce2e724df3fc98d62719189f54d5fc83cc7fc
Eric Brelsford <ebrelsford@gmail.com>
2015-09-09 10:42:59 -0400

    Better admin for can requests

--- curbyourlitter_alley/canrequests/admin.py
+++ curbyourlitter_alley/canrequests/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import CanRequest
 
-admin.site.register(CanRequest)
+
+class CanRequestAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'added', 'can_type', 'can_subtype', 'name', 'email',)
+    list_filter = ('can_type', 'can_subtype', 'added',)
+
+
+admin.site.register(CanRequest, CanRequestAdmin)
